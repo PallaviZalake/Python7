4dedfd747e85a5af166085813f9e8a5c965ee453
PallaviZalake <plv753@gmail.com>
2022-10-31 20:57:33 +0530

    calculator code

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def Addition(a,b):
+    return a+b
+
+def Substraction(a,b):
+    return a-b
+
+def Multipliction(a,b):
+    return a*b
+
+def Division(a,b):
+    return a/b
+
+print("Additoin is:",Addition(10,20))
+print("Substraction is:",Substraction(40,20))
+print("Multipliction is:",Multipliction(10,20))
+print("Division is:",Division(50,5))
