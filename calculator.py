41f8bd357e9298ad652c9001bfde1c2b7804b8f6
PallaviZalake <plv753@gmail.com>
2022-11-01 00:10:27 +0530

    floor division code added

--- calculator.py
+++ calculator.py
@@ -10,7 +10,11 @@ def Multipliction(a,b):
 def Division(a,b):
     return a/b
 
+def Floor_division(a,b):
+    return a//b
+
 print("Additoin is:",Addition(10,20))
 print("Substraction is:",Substraction(40,20))
 print("Multipliction is:",Multipliction(10,20))
 print("Division is:",Division(50,5))
+print("Floor Division is:",Floor_division(50,5))
