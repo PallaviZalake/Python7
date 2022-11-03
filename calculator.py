4d7102ee3a1290ff4860e5c5f75eb19616b8cb90
PallaviZalake <plv753@gmail.com>
2022-11-03 20:59:18 +0530

    new code

--- calculator.py
+++ calculator.py
@@ -13,13 +13,6 @@ def Division(a,b):
 def Floor_division(a,b):
     return a//b
 
-def fact(n):
-    if n == 0:
-        res = 1
-    else:
-        res = (n * fact(n - 1))
-    return res
-
 def reverse_str(m):
     return m[::-1]
 
@@ -29,7 +22,7 @@ print("Substraction is:",Substraction(40,20))
 print("Multipliction is:",Multipliction(10,20))
 print("Division is:",Division(50,5))
 print("Floor Division is:",Floor_division(50,5))
-print(fact(5))
+
 print("Reverse string is:",reverse_str("Python"))
 
 print("fsghfhjdhjdghjdghj")
